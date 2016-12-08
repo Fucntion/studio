35aa9e4a0796c725f10ac338a1b1fcba58dc09f8
function <function@icloudinn.com>
2016-12-08 22:30:18 +0800

    完善了origin
    觉得elment还有改进的空间，多逛逛segmentfault

--- src/view/room/config/common/feature.vue
+++ src/view/room/config/common/feature.vue
@@ -1,6 +1,21 @@
 <template>
-	<div>
-		style
+	<div class="feature">
+		 <div class="header">
+		 	<span class="header_tab" :class="showStatus.cover?'active':''"  @click="changeShow('cover')">封面</span>
+		 <span class="header_tab" :class="showStatus.countDown ?'active':''" @click="changeShow('countDown')">倒计时</span>
+		 <span class="header_tab" :class="showStatus.logo ?'active':''" @click="changeShow('logo')">logo</span>
+		 </div>
+
+		<div v-if="showStatus.cover">
+			封面
+		</div>
+		<div v-if="showStatus.countDown">
+			倒计时
+		</div>
+		<div v-if="showStatus.logo">
+			logo
+		</div>
+
 	</div>
 </template>
 
@@ -9,11 +24,26 @@
 		data: function() {
 
 			return {
+				showStatus:{
+					cover:true,
+					logo:false,
+					countDown:false
 
+				}
 			}
 		},
 		methods: {
+			changeShow:function(name){
 
+				for(var k in this.showStatus){
+
+					if(k ==name){
+						this.showStatus[k] =true;
+						continue;
+					}
+					this.showStatus[k] =false;
+				}
+			}
 		},
 		components: {
 
--- src/view/room/config/common/origin.vue
+++ src/view/room/config/common/origin.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="origin">
-		 <button @click="changeShow('origin')">直播源</button>
-		 <button @click="changeShow('total')">人数限制</button>
-		<div class="hr"></div>
-		<div v-if="origin">
+		 <div class="header">
+		 	<span class="header_tab" :class="showStatus.origin ?'active':''"  @click="changeShow('origin')">直播源</span>
+		 <span class="header_tab" :class="showStatus.total ?'active':''" @click="changeShow('total')">人数限制</span>
+		 </div>
+
+		<div v-if="showStatus.origin">
 			<div><label for="name">直播间名称</label><input type="text" name="room" id="name"></div>
 			<div><label for="allowNum">最大观看人数</label><input type="text" name="allow" id="allowNum"></div>
 			<button class="sub">保存</button>
@@ -13,10 +15,10 @@
 			<a href="http://run.mockplus.cn/NxbG2gpXNfjX2aJh/index.html">http://run.mockplus.cn/NxbG2gpXNfjX2aJh/index.html</a><br/> 扫我预览
 			<a href="http://cli.im/">二维码美化</a>
 		</div>
-		<div v-if="total">
+		<div v-if="showStatus.total">
 			<div><label for="name">直播间名称</label><input type="text" name="room" id="name"></div>
 			<div><label for="allowNum">最大观看人数</label><input type="text" name="allow" id="allowNum"></div>
-			<button class="sub">保存</button>
+			<button class="sub">保存222</button>
 			<div class="hr"></div>
 			推流地址
 			<a href="http://run.mockplus.cn/NxbG2gpXNfjX2aJh/index.html">http://run.mockplus.cn/NxbG2gpXNfjX2aJh/index.html</a><br/> 预览地址
@@ -43,7 +45,7 @@
 				for(var k in this.showStatus){
 
 					if(k ==name){
-						this.showStatus[k] =ture;
+						this.showStatus[k] =true;
 						continue;
 					}
 					this.showStatus[k] =false;
