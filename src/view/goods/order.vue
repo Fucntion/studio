0755bf83f590447832223d09d91f4c7e6e4d4a42
icloudinn <271938312@qq.com>
2017-02-27 09:42:34 +0800

    20170227
    开始卖钱

--- src/view/goods/order.vue
+++ src/view/goods/order.vue
@@ -7,7 +7,8 @@
 
 					<img style="max-width: 100px;" :src="row.goodsImg" />
 					<div class="info">{{row.goodsName}}
-						<h2>{{row.shopPrice}}</h2></div>
+						<h2>{{row.shopPrice}}</h2>
+					</div>
 				</div>
 			</el-table-column>
 			<el-table-column prop="number" min-width="50px" align="center" label="数量"></el-table-column>
@@ -15,33 +16,33 @@
 
 			<el-table-column prop="total" min-width="60px" align="center" label="总价(￥)"></el-table-column>
 			<el-table-column inline-template align="center" min-width="80px" label="联系人">
-				<div style="text-align:center;">{{row.linkName}}<br/>{{row.tel}}</div>
+				<div style="text-align:center;">{{row.userName}}<br/>{{row.tel}}</div>
 			</el-table-column>
 			<el-table-column inline-template min-width="100px" label="操作" align="center">
 				<template>
 					<el-button>{{row.orderStatus}}</el-button>
-
 				</template>
 
 			</el-table-column>
 		</el-table>
 		<div class="control">
-			<el-pagination class="audience_total" @size-change="handleSizeChange" @current-change="handleCurrentChange"  :page-size="20" layout="total, sizes, prev, pager, next, jumper" :total="total">
-			</el-pagination>
-			<!-- <el-button class="export">邮件发送</el-button> -->
-			<!-- <el-button class="export" type="primary">保存到本地</el-button> -->
+			<el-pagination class="audience_total" @size-change="handleSizeChange" @current-change="handleCurrentChange" :page-size="20"
+				layout="total, sizes, prev, pager, next, jumper" :total="total">
+				</el-pagination>
+				<!-- <el-button class="export">邮件发送</el-button> -->
+				<!-- <el-button class="export" type="primary">保存到本地</el-button> -->
 		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		data: function() {
+		data: function () {
 
 			return {
 				orderDataList: [],
-				loading:true,
-				total:null
+				loading: true,
+				total: null
 			}
 		},
 		methods: {
@@ -54,26 +55,15 @@
 			handleCurrentChange(val) {
 				this.currentPage = val;
 			},
-			init:function(){
-				var url ="shop=http://shop.icloudinn.com/index.php/Api/Orders/queryShopOrders--token--&currPage=1&pageSize=20"
-				this.$http.get(url).then((response)=>{
-					// {
-					// 	orderId: 89757,
-					// 	goodsName: '阳光MAX能理疗的远红外碳管电热水暖器',
-					// 	shopPrice: '2016', //商品价格
-					// 	number: 10,
-					// 	address: '海口市演丰镇瑶城村西河路1号',
-					// 	linkName: '汪君相',
-					// 	tel: '15607683921',
-					// 	total: 255,
-					// 	goodsImg: 'http://saaslive.oss-cn-shanghai.aliyuncs.com/%E9%9D%92%E4%BA%91/TB1TGZ_OpXXXXcLXXXXSutbFXXX.jpg_q90.jpg',
-					// 	orderStatus: '已支付'
-					// }
-					this.orderDataList = response.body.data.root
-					this.total = response.body.data.total
+			init: function () {
+				var url = "shop=http://shop.icloudinn.com/index.php/Api/Orders/queryShopOrders--token--&currPage=1&pageSize=20"
+				this.$http.get(url).then((response) => {
 
-					this.loading=false
-				},(response)=>{
+					this.orderDataList = response.body.data.root
+					this.total = parseInt(response.body.data.total)
+
+					this.loading = false
+				}, (response) => {
 
 				})
 			}
@@ -81,10 +71,11 @@
 		components: {
 
 		},
-		mounted(){
+		mounted() {
 			this.init()
 		}
 	}
+
 </script>
 
 <style lang="less">
